4793c884cceb93d84ddd39704aa9d15ac33698e3
Sophie Bradshaw <sophie@madebyfudge.com>
2011-05-08 10:37:36 +0100

    new files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '670dba40ce43632ccbaaef63f65b14d54872d9b6cdb3774235bf15cd5c620f41aa707245c23e774563d7cb526788373b191bb9d549259a81e4870e12f50fa16d'
---
+++ db/migrate/20110508092656_create_foods.rb
@@ -0,0 +1,14 @@
+class CreateFoods < ActiveRecord::Migration
+  def self.up
+    create_table :foods do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :foods
+  end
+end
